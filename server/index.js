0be914ffbd63a3944f0d8278bb923f53cfff2b7a
ThomasDziurdzi <thomasdziurdzi@gmail.com>
2024-07-31 11:41:16 +0200

    creating route GET /

--- server/index.js
+++ server/index.js
@@ -12,6 +12,12 @@ const app = require("./app/config");
 // Get the port from the environment variables
 const port = process.env.APP_PORT;
 
+
+app.get("/", (req,res) => {
+  res.status(200).send("Welcome to Wild Series !")
+})
+
+
 // Start the server and listen on the specified port
 app
   .listen(port, () => {
